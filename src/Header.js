0b5e9656936034f765f835c486c2ce112bf20c58
MohdHanoti <mohadalhanoty@gmail.com>
2022-08-21 22:20:12 +0300

    new21/8

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component{
+    render(){
+    return(
+        <div>
+        <p>well be combleted next day</p>
+        </div>
+    )
+    }
+}
+
+export default Header;
